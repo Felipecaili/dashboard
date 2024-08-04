169493494b21ab8ca24d133fae480ad74528ca27
Felipecaili <67855287+Felipecaili@users.noreply.github.com>
2024-08-04 11:13:10 -0500

    Ultimo tutorial antes de DB

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <p>Whte and Empty Dashboard Page</p>;
+  }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Montserrat, Tac_One, Lusitana} from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin'], weight: '400'})
+
+export const tacOne = Tac_One({subsets: ['latin'], weight: ['400']})
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['400']})
